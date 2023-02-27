46e3cb81738cf62360773cde63dab5cdd7e6d827
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2023-02-26 19:38:16 -0800

    Update Exercise_4.py

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,11 +1,41 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+  if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort left and right halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merge the sorted halves
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
   
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+    print(arr)
     #write your code here
   
 # driver code to test the above code 
